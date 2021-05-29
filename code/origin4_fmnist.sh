43bd34284bd80b0a3c5900036cd44757ab4813ff
Charl0tte19 <84913489+Charl0tte19@users.noreply.github.com>
2021-05-30 06:39:50 +0800

    Delete origin4_fmnist.sh

--- code/origin4_fmnist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ratio_list_scale="0.01 0.02 0.03 0.04 0.05"
-for ratio4 in $ratio_list_scale
-do
-	python -u main_origin_fmnist.py --gpu 0 --seed 32 --scale --epoch 100 --noniid 0.8 --attack_mode="poison" --attack_ratio ${ratio4} --test_label_acc --target_random --dataset="fmnist" --model_path="./fmnist/origin/noniid_0.8/ratio_${ratio4}/poison_${ratio4}_Scale_0.pt" 2>&1 | tee ./fmnist/origin/noniid_0.8/ratio_${ratio4}/poison_${ratio4}_Scale_0.txt
-
-done	
