53bdb7fbf91a7e51078d98aba2e546578bc2f4b8
Charl0tte19 <84913489+Charl0tte19@users.noreply.github.com>
2021-05-30 06:40:19 +0800

    Delete shuffle3_fmnist.sh

--- code/shuffle3_fmnist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ratio_list_scale="0.01 0.02 0.03 0.04 0.05"
-for ratio3 in $ratio_list_scale
-do
-	python -u main_shuffle_fmnist.py --gpu 0 --seed 32 --dataset="fmnist" --scale --epoch 20 --noniid 0.4 --attack_mode="poison" --attack_ratio ${ratio3} --test_label_acc --target_random --shuffle --model_path="./fmnist/noniid_0.4/ratio_${ratio3}/poison_${ratio3}_Scale_0.pt" 2>&1 | tee ./fmnist/noniid_0.4/ratio_${ratio3}/poison_${ratio3}_Scale_0.txt
-
-done
