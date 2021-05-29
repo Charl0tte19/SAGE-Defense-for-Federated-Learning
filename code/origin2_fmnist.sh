0df2006b5a2908fd5c4d50a2025e27d34385a150
Charl0tte19 <84913489+Charl0tte19@users.noreply.github.com>
2021-05-30 06:28:44 +0800

    Add files via upload

---
+++ code/origin2_fmnist.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ratio_list="0.1 0.2 0.3 0.4 0.5"
+for ratio2 in $ratio_list
+do
+	python -u main_origin_fmnist.py --gpu 0 --seed 32 --epoch 100 --noniid 0.8 --attack_mode="poison" --attack_ratio ${ratio2} --test_label_acc --target_random --dataset="fmnist" --model_path="./fmnist/origin/noniid_0.8/ratio_${ratio2}/poison_${ratio2}_notScale_0.pt" 2>&1 | tee ./fmnist/origin/noniid_0.8/ratio_${ratio2}/poison_${ratio2}_notScale_0.txt
+
+done
