2ff2bcd6de26a7f5fa049bc6677b998710cd1255
Charl0tte19 <84913489+Charl0tte19@users.noreply.github.com>
2021-05-30 07:00:21 +0800

    Add files via upload

---
+++ code/sh_scripts_mnist/after2_mnist.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ratio_list="0.1 0.2 0.3 0.4 0.5"
+
+loc="/home/ubuntu/My_FL/src/mnist/seed_32/command"
+
+seed="seed_32"
+
+for i in $ratio_list
+do
+	c=$(cat ${loc}/0.8${i}com.txt)
+	#cmd=$c"2>&1|tee ./mnist/noniid_0.8/ratio_"$i"/log.txt"
+	$c 2>&1 | tee ./mnist/${seed}/noniid_0.8/ratio_${i}/log.txt
+done
+
+
