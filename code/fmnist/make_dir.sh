ac365b6cbed4c53ddc26d420bb3d918963eb0040
Charl0tte19 <84913489+Charl0tte19@users.noreply.github.com>
2021-12-03 02:59:20 +0800

    Update make_dir.sh

--- code/fmnist/make_dir.sh
+++ code/fmnist/make_dir.sh
@@ -4,9 +4,9 @@ list="0.1 0.2 0.3 0.4 0.5 0.01 0.02 0.03 0.04 0.05"
 
 for i in $list
 do
-	mkdir ratio_$i
+	mkdir ./fmnist/ratio_$i
 done
 
-mkdir noniid_0.4
-mv ra* noniid_0.4/
-cp -r noniid_0.4 noniid_0.8
+mkdir ./fmnist/noniid_0.4
+mv ra* ./fmnist/noniid_0.4/
+cp -r ./fmnist/noniid_0.4 ./fmnist/noniid_0.8
